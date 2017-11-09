38ae3e196b9b141416a4c46f65f35d3857edde99
MUHAMMAD SALMAN BIN MOHD SHAH <msalman26@live.utm.my>
2017-11-09 14:00:48 +0800

    Added Mail Plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -52,6 +53,7 @@ import { ApplyPage } from '../pages/zakat/apply/apply';
   providers: [
     StatusBar,
     SplashScreen,
+    EmailComposer,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/zakat/apply/apply.ts
+++ src/pages/zakat/apply/apply.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
-import { PaymentPage } from '../../payment/payment';
+import { EmailComposer } from '@ionic-native/email-composer';
+
+//import { PaymentPage } from '../../payment/payment';
 
 @IonicPage()
 @Component({
@@ -10,11 +12,27 @@ import { PaymentPage } from '../../payment/payment';
 })
 export class ApplyPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl:ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl:ViewController, private emailComposer: EmailComposer) {
   }
 
   onClose(remove = false){
     this.viewCtrl.dismiss(remove);
   }
+
+  onApply() {
+
+    let email = {
+    to: 'sarumanokun@gmail.com',
+    cc: 'msalmanshah26@gmail.com',
+    bcc: ['', ''],
+    attachments: [ ],
+    subject: 'Hi there',
+    body: 'How are you? Nice greetings from Detik Ideal',
+    isHtml: true
+    };
+
+    this.emailComposer.open(email);
+  }
+
 
 }
